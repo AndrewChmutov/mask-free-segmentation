edb30bb644f78b7a8c80ad48579cb233630a871c
Andrew Chmutov <ulman.andre@gmail.com>
2024-12-31 23:39:47 +0100

    Hotfix v3
    Signed-off-by: Andrew Chmutov <ulman.andre@gmail.com>

--- segmentation/utils.py
+++ segmentation/utils.py
@@ -5,6 +5,6 @@ from pathlib import Path
 def get_model_path(name: str, load: bool) -> str:
     path = f"models/{name}.pth"
     if not Path(path).is_file() and "KAGGLE_KERNEL_RUN_TYPE" in os.environ and load:
-        path = f"/kaggle/input/{name}/pytorch/default/{name}.pth"
+        path = f"/kaggle/input/{name}/pytorch/default/1/{name}.pth"
     assert Path(path).is_file() or not load, f"Couldn't find a model in {path}"
     return path
